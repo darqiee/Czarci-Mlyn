f2f76f9a878f83fdc598b91a44e098aa8b5cd46a
darqiee <darqieee@gmail.com>
2025-07-13 14:21:10 +0200

    Another update, gallery done

--- Czarci Młyn Świeradów Zdrój/script.js
+++ Czarci Młyn Świeradów Zdrój/script.js
@@ -37,3 +37,15 @@ dots.forEach((li, key) => {
 window.onresize = function(event) {
     reloadSlider();
 };
+
+const showMenu = (toggleId, navId) =>{
+   const toggle = document.getElementById(toggleId),
+         nav = document.getElementById(navId)
+
+   toggle.addEventListener('click', () =>{
+       nav.classList.toggle('show-menu')
+       toggle.classList.toggle('show-icon')
+   })
+}
+
+showMenu('nav-toggle','nav-menu')
